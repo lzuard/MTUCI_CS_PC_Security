644ca7c33a15d5f68a7541dc430ea43ed5b70925
Izuard <sasha.20133@gmail.com>
2022-10-31 15:46:20 +0300

    Добавьте файлы проекта.

---
+++ MTUCI_CS_PC_Security/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Windows;
+
+namespace MTUCI_CS_PC_Security
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
